660e6cb956799136e036509ef34be756ca25e5cd
GabrielRot44 <159585458+GabrielRot44@users.noreply.github.com>
2026-03-07 11:44:00 -0300

    Eu criei o Formulario que solicita dois números, e criei o php para receber os valores do formulário, somar-los e exibir a soma.

---
+++ Exercicios/Exercicio2/Form1/Exercicio1.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 2</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+
+<body> 
+<div class="container py-3">
+
+<form method="post" action="">
+            <div class="mb-3">
+              <label for="number" class="form-label ">Informe o primeiro número</label>
+              <input type="number" id="number" name="number" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="number2" class="form-label">Informe o segundo número</label>
+                <input type="number" id="number2" name="number2" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Somar</button>
+</form>
+
+<?php 
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $number = $_POST["number"];
+        $number2 = $_POST["number2"];
+        $soma = $number + $number2;
+        echo "<p>A soma dos números $number + $number2 = $soma</p>";}
+    else {
+        echo "<p>Por favor, preencha o formulário acima.</p>";
+    }
+
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+</div>
+</body>
+</html>
+
+
