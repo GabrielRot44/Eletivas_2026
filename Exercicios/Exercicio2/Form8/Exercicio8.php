3c95d6ba5c462fcc529bcaaa9904d83ebb1b076e
GabrielRot44 <159585458+GabrielRot44@users.noreply.github.com>
2026-03-10 20:34:41 -0300

    Fiz um formulário para coletar a altura e largura de um retângulo e calcular sua área.

---
+++ Exercicios/Exercicio2/Form8/Exercicio8.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 2</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+<style>
+    .font-bold{
+        font-weight: bold;
+    }
+</style>
+</head>
+
+<body> 
+<div class="container py-3">
+
+<form method="post" action="">
+            <div class="mb-3">
+              <label for="altura" class="form-label ">Altura:</label>
+              <input type="number" id="altura" name="altura" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="largura" class="form-label ">Largura:</label>
+              <input type="number" id="largura" name="largura" class="form-control" required="">
+            </div>
+
+<button type="submit" class="btn btn-primary">Fazer área</button>
+</form>
+
+<?php 
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $altura = $_POST["altura"];
+        $largura = $_POST["largura"];
+        $area = $altura * $largura;
+        echo ("
+        <div class='container py-3 text-center font-bold'>
+        <p>A área desse retângulo de $altura x $largura é $area</p>
+        </div>
+        ");
+    }
+
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+</div>
+</body>
+</html>
+
+
