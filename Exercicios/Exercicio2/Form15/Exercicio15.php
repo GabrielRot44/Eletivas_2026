c2ecec43297f6d14750114ec13826fa686d5dcdb
GabrielRot44 <159585458+GabrielRot44@users.noreply.github.com>
2026-03-10 21:29:58 -0300

    Fiz um formulário para coletar a altura e o pesso de uma pessoa e calcular seu IMC.

---
+++ Exercicios/Exercicio2/Form15/Exercicio15.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 2</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+<style>
+    .font-bold{
+        font-weight: bold;
+    }
+</style>
+</head>
+
+<body> 
+<div class="container py-3">
+
+<form method="post" action="">
+            <div class="mb-3">
+              <label for="peso" class="form-label ">Peso:</label>
+              <input type="number" step="any" id="peso" name="peso" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+              <label for="altura" class="form-label ">Altura em metros:</label>
+              <input type="number" step="any" id="altura" name="altura" class="form-control" required="">
+            </div>
+
+<button type="submit" class="btn btn-primary">Converter</button>
+</form>
+
+<?php 
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $peso = $_POST["peso"];
+        $altura = $_POST["altura"];
+        $imc = $peso / ($altura ** 2);
+        if($altura > 3){
+            echo"<div class='container py-3 text-center font-bold'>infome sua altura em metros!</div>";
+        }
+        else{
+            echo("
+            <div class='container py-3 text-center font-bold'>
+            <p>Seu IMC = $imc.</p>
+            </div>
+            ");
+        }
+    }
+
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+</div>
+</body>
+</html>
+
+
