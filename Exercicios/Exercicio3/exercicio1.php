dc5a86f8f83946a2d4b3f64cab284dba9903f87f
GabrielRot44 <159585458+GabrielRot44@users.noreply.github.com>
2026-03-17 21:03:08 -0300

    eu fiz um formulário para coletar 7 valores numericos, e começei verificando se havia números repetidos entre os valores, e logo após busquei o menor valor e sua posição no vetor e exibi-as

---
+++ Exercicios/Exercicio3/exercicio1.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 2</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+<style>
+    .font-bold{
+        font-weight: bold;
+    }
+</style>
+</head>
+
+<body> 
+<div class="container py-3">
+
+<form method="post" action="">
+                <div class="row inline-row mb-3">
+                    <?php
+                        for ($i = 1; $i <= 7; $i++) {
+                            echo "  <div class='col-md-1'> <label for='numero$i' class='form-label'>$i º Número:</label>
+                                    <input type='number' step='any' id='numero$i' name='numeros[]' class='form-control' required>
+                                    </div>";
+                        }
+                    ?>
+                </div>
+
+<button type="submit" class="btn btn-primary">Calcular</button>
+</form>
+
+<?php 
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $numeros = $_POST["numeros"];
+        $repetido = false;
+        for ($i = 0; $i < 7; $i++) {
+            for ($o = $i +1; $o <7; $o++) {
+                if ($numeros[$i] == $numeros[$o]) {
+                    $repetido = true;
+                }
+            }
+        }
+        if ($repetido) {
+            echo("
+            <div class='container py-3 text-center font-bold'>
+            <p>Os números devem ser diferentes.</p>
+            </div>
+            ");
+            return;
+        }
+            else{
+                $menor = 0;
+                $posicao = 0;
+                for ($i = 0 ; $i < 7; $i++) {
+                    if ($numeross[$i] < $menor) {
+                        $menor = $numeros[$i];
+                        $posicao = $i;
+                    }
+                }
+        }
+        echo("
+        <div class='container py-3 text-center font-bold'>
+        <p>O menor número é $menor e ele esta na posição $posicao</p>
+        </div>
+        ");
+    }
+
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+</div>
+</body>
+</html>
+
+
