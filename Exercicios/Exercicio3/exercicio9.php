f9f5e84455f6bb0e898f85e2ff16c421d608a978
GabrielRot44 <159585458+GabrielRot44@users.noreply.github.com>
2026-03-23 12:41:06 -0300

    Formulário para coletar um número e exibir o fatorial do mesmo.

---
+++ Exercicios/Exercicio3/exercicio9.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 2</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+<style>
+    .font-bold{
+        font-weight: bold;
+    }
+</style>
+</head>
+
+<body> 
+<div class="container py-3">
+
+<form method="post" action="">
+    <div class="row inline-row mb-3">                
+    <div class='col-md'> <label for='numero' class='form-label'>Número</label>
+    <input type='number' step='any' id='numero' name='numero' class='form-control' required>
+    </div>
+    </div>
+<div class="text-center">
+    <button type="submit" class="btn btn-primary mb-3">Calcular</button>
+</div>
+</form>
+<!--  Crie um formulário para que o usuário informe um número. Use um loop
+for para calcular o fatorial desse número e exibir o resultado. -->
+<?php 
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $numero = $_POST["numero"];
+        $fatorial = 1;
+        for ($i = 1; $i <= $numero; $i++) {
+            $fatorial *= $i;
+        }
+        echo("<div class='row inline-row mb-3 container py-3 text-center font-bold'>");
+        echo("<p class='col-1'>Fatorial de $numero = $fatorial</p>");
+        echo("</div>");
+    }
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+</div>
+</body>
+</html>
+
+
