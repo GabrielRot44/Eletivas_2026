17190df55ab236f7aa41a5ab8b080d0e24a9783e
GabrielRot44 <159585458+GabrielRot44@users.noreply.github.com>
2026-04-03 16:46:19 -0300

    formulario coleta uma frase e exibe quantas vogais há nela

---
+++ Exercicios/Exercicio4/exercicio8.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 3</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+<style>
+    .font-bold{
+        font-weight: bold;
+    }
+</style>
+</head>
+
+<body> 
+<div class="container py-3">
+
+<form method="post" action="">
+    <div class="row inline-row mb-3">                
+    <div class='col-md'> <label for='frase' class='form-label'>Frase:</label>
+    <input type='text' id='frase' name='frase' class='form-control' required>
+    </div>
+    </div>
+<div class="text-center">
+    <button type="submit" class="btn btn-primary mb-3">Calcular</button>
+</div>
+</form>
+<!-- Crie um programa em PHP que leia uma frase e apresente quantas vogais existem na frase-->
+<?php 
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $frase = $_POST["frase"];
+        $vogais = "aeiouAEIOU";
+        $contagem = 0;
+
+        for ($i = 0; $i < strlen($frase); $i++) {
+            if (strpos($vogais, $frase[$i]) !== false) {
+                $contagem++;
+            }
+        }
+        echo("<div class='row inline-row mb-3 container py-3 text-center font-bold'>");
+        echo("<p>A frase '$frase' contém $contagem vogais.</p>");
+        echo("</div>");
+    }
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+</div>
+</body>
+</html>
+
+
